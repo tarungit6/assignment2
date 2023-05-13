5343df989497fd84e742401e8108af7b8548b6ad
tarungit6 <tarungabbar9642@gmail.com>
2023-05-13 23:40:40 +0530

    updating hello world program to a simple palindrome program

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("hello world")
+def is_palindrome(word):
+    # Remove whitespace and convert to lowercase
+    word = word.replace(" ", "").lower()
+    
+    # Check if the word is equal to its reverse
+    if word == word[::-1]:
+        return True
+    else:
+        return False
+
+# Test the function
+input_word = input("Enter a word: ")
+if is_palindrome(input_word):
+    print(f"{input_word} is a palindrome!")
+else:
+    print(f"{input_word} is not a palindrome.")
